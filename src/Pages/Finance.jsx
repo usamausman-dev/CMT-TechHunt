4d92746328d83b0b20d73809996709368a5a81a4
usama <uusman004@gmail.com>
2023-02-21 00:10:36 +0500

    FEAT:Add Finance Page UI

--- src/Pages/Finance.jsx
+++ src/Pages/Finance.jsx
@@ -1,11 +1,154 @@
 import React from 'react'
-import {MonthlyGraph} from '../components'
+import { MonthlyGraph } from '../components'
+import CardImg from '../assests/cards.png'
 
 const Finance = () => {
   return (
-    <div>
-      <MonthlyGraph/>
-    </div>
+    <>
+      <div className='grid grid-cols-3 gap-8'>
+        <div className='shadow-xl rounded-xl p-7 h-[500px]'>
+          <h1 className='font-bold text-xl'>Sales <br /> Statistics</h1>
+          <div className='my-4'>
+            <h2>Sales</h2>
+            <p className='text-lg font-semibold'>$20,000.00</p>
+          </div>
+          <MonthlyGraph />
+        </div>
+
+        <div className='col-span-2 p-7 shadow-xl rounded-xl h-[500px] overflow-y-auto'>
+          <div className='flex justify-between items-center '>
+            <div>
+              <h1 className='text-xl font-bold'>List of Clients</h1>
+            </div>
+
+            <div className='flex items-center'>
+              <input className='bg-gray-200 px-3 py-1 rounded-md' type="text" placeholder='Search' />
+              <i class="fa-solid fa-magnifying-glass mx-2 text-gray-400"></i>
+              <i class="fa-solid fa-sliders mx-2 text-gray-400"></i>
+              <i class="fa-solid fa-filter mx-2 text-gray-400"></i>
+
+            </div>
+
+
+          </div>
+
+          <div className='w-100 my-6'>
+            <table width="100%" className='text-center border-separate border-spacing-y-3.5'>
+              <thead className=''>
+                <tr>
+                  <th>Client</th>
+                  <th>Project</th>
+                  <th>Assign To</th>
+                  <th>Date</th>
+                  <th>Sales</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+
+                <tr>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td>Lorem Epsum</td>
+                  <td className='text-green-400'>1,000,000</td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+        </div >
+
+
+
+      </div>
+
+      <div className='shadow-xl mt-4 w-[300px] rounded-xl p-7'>
+        <div className='flex justify-between'>
+          <h1 className='text-xl font-bold'>Balances</h1>
+          <button className='bg-[#74c27b] text-white px-2 rounded-lg'><i class="fa-solid fa-plus"></i></button>
+        </div>
+        <div className='flex items-center'>
+          <span className='text-[#74c27b] text-xl font-bold mr-1'>$</span><span>Dollar</span>
+        </div>
+        <h1 className='text-[#74c27b] text-3xl font-semibold'>9784.79</h1>
+
+        <img src={CardImg} alt="atm-cards" />
+
+      </div>
+    </>
   )
 }
 
--- src/components/MonthlyGraph.jsx
+++ src/components/MonthlyGraph.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { XAxis, YAxis, Tooltip, AreaChart, Area, ResponsiveContainer } from 'recharts';
 
 const monthlyGraph = () => {
+  const data = [
+    { name: 'Jan', uv: 400, pv: 2400, amt: 2400 },
+    { name: 'Feb', uv: 100, pv: 2800, amt: 2800 },
+    { name: 'Mar', uv: 600, pv: 2900, amt: 2900 },
+    { name: 'April', uv: 200, pv: 2900, amt: 2900 },
+    { name: 'May', uv: 700, pv: 2900, amt: 2900 },
+    { name: 'Jun', uv: 350, pv: 2900, amt: 2900 },
+    { name: 'Jul', uv: 650, pv: 2900, amt: 2900 },
+    { name: 'Aug', uv: 800, pv: 2900, amt: 2900 },
+    { name: 'Sep', uv: 100, pv: 2900, amt: 2900 },
+    { name: 'Oct', uv: 200, pv: 2900, amt: 2900 },
+    { name: 'Nov', uv: 400, pv: 2900, amt: 2900 },
+    { name: 'Dec', uv: 300, pv: 2900, amt: 2900 },
+  ];
+
   return (
-    <div>
-      Graph
-    </div>
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart width={200} height={400} data={data} syncId="anyId" margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stroke="#95cc9a" fill="#82ca9d" />
+      </AreaChart>
+    </ResponsiveContainer>
   )
 }
 
