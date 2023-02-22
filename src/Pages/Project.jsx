65fa3705ba7ab3eed9d98a604da8bdf46a2f91e0
usama <uusman004@gmail.com>
2023-02-23 04:16:47 +0500

    FEAT:Add Create DropDown Project Dynamic Page

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -46,6 +50,17 @@ export default function VerticalTabs() {
   };
 
 
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   const projects = ['HRM', 'SQA', 'Frontend', 'Backend', 'Marketing']
 
   return (
@@ -56,25 +71,30 @@ export default function VerticalTabs() {
           <i className="fa-solid fa-magnifying-glass mx-2 text-gray-300"></i>
           <input className='bg-slate-100 px-3 py-1 rounded-md' type="text" placeholder='Search' />
           <i className="fa-solid fa-filter mx-2 text-gray-300"></i>
-
         </div>
 
-        <div className='mx-6'><button className='bg-[#72c179] font-semibold rounded-lg text-white px-8 py-2'>Create <i class=" ml-4 fa-solid fa-plus"></i></button></div>
-        
+        <div>
+          <Button sx={{ background: '#72c179', fontWeight: 700, color: 'white', padding: '10px 20px' }} id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick}>
+            Create <i class="ml-3 fa-solid fa-plus"></i>
+          </Button>
+
+          <Menu id="basic-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{ 'aria-labelledby': 'basic-button' }}>
+            <MenuItem onClick={handleClose}>New Task</MenuItem>
+            <MenuItem onClick={handleClose}>Sub-Task</MenuItem>
+          </Menu>
+        </div>
+
+
       </div>
       <Box className="shadow-2xl" sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '67vh', borderRadius: '20px' }}>
         <Tabs className='py-6' TabIndicatorProps={{ style: { backgroundColor: "#72c179", width: '5px' } }} orientation="vertical" variant="standard" value={value} onChange={handleChange} aria-label="Vertical tabs example" sx={{ borderRight: 1, borderColor: 'divider', width: '250px' }}>
-
           {
             projects.map((val, key) => <Tab label={val} {...a11yProps({ key })} />)
           }
-
         </Tabs>
 
         {
-          projects.map((val, key) => <TabPanel value={value} index={key}>
-            {val}
-          </TabPanel>)
+          projects.map((val, key) => <TabPanel value={value} index={key}>{val}</TabPanel>)
         }
       </Box>
     </div>
